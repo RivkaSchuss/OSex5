6be1678f5c544d8f1cf39adc40ffbc5b50519bdf
Rivka Schuss <rschuss123@gmail.com>
2018-06-11 18:31:34 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/OSex5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSex5.dir/ex51.c.o"
+  "CMakeFiles/OSex5.dir/getChar.c.o"
+  "CMakeFiles/OSex5.dir/ex52.c.o"
+  "OSex5.pdb"
+  "OSex5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OSex5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ex51.c
@@ -0,0 +1,8 @@
+//Rivka Schuss 340903129
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
